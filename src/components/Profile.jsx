781932a8674b7f9b17397dc8f6cb00ade5445d13
mallexibra <mallexibra@gmail.com>
2023-04-08 20:27:02 +0700

    Memperbaiki image lagi

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,7 @@
 function Profile() {
   return (
     <div className='flex justify-center items-center'>
-      <div className='bg-[url("src/img/profile.jpg")] w-32 h-32 bg-cover bg-[-80px] rounded-full border-teal-500 border-solid border-4 m-5'></div>
+      <div className='bg-[url("/static/img/profile.jpg")] w-32 h-32 bg-cover bg-[-80px] rounded-full border-teal-500 border-solid border-4 m-5'></div>
     </div>
   );
 }
--- src/image.js
+++ src/image.js
@@ -1,17 +1,17 @@
 const IMAGES = [
   {
     id: 1,
-    images: "src/img/sertif1.png",
+    images: "/static/img/sertif1.png",
     description: "Belajar Dasar Pemrograman Web",
   },
   {
     id: 2,
-    images: "src/img/sertif2.png",
+    images: "/static/img/sertif2.png",
     description: "Belajar Membuat Front-end Web Untuk Pemula",
   },
   {
     id: 3,
-    images: "src/img/sertif3.png",
+    images: "/static/img/sertif3.png",
     description: "Belajar Dasar Pemrograman Javascript",
   },
 ];
